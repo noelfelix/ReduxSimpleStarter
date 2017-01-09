9ba5e13aa75b1a30c91d79e1847b5ccfb2656e57
Noel Felix <noel.r.felix@gmail.com>
2017-01-09 00:03:03 -0800

    gfwreg

--- src/containers/book-list.js
+++ src/containers/book-list.js
@@ -1,6 +1,11 @@
-import React, { Component} from 'react'
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
 
-export default class BookList extends Component {
+class BookList extends Component {
+
+  renderList() {
+    return this.props.books.map(book => <li className="list-group-item" key={book.title}>{book.title}</li>)
+  }
 
   render() {
     return (
@@ -9,8 +14,12 @@ export default class BookList extends Component {
       </ul>
     )
   }
+}
 
-  renderList() {
-    return this.props.books.map(book => <li className="list-group-item" key={book.title}>{book.title}</li>)
+function mapStateToProps(state) {
+  return {
+    books: state.books
   }
 }
+
+export default connect(mapStateToProps)(BookList)
